1dba7f248cf2358d2bfd16c2d4e7f00f8196f0aa
Nick Wing <git@mythgaming.net>
2020-03-27 13:42:23 +0000

    added a couple more tests for eachConcurrent

--- test/async.ts
+++ test/async.ts
@@ -4,17 +4,28 @@ import { eachConcurrent, sleep } from '../src'
 
 describe('async utils', () => {
   const items = Array.from(Array(20).keys())
+  const sleeptime = 25
   it('eachConcurrent should go through a list of items with a max concurrency of 5', async () => {
     const starttime = new Date()
-    await eachConcurrent(items, 5, () => sleep(50))
+    await eachConcurrent(items, 5, () => sleep(sleeptime))
     const endtime = new Date()
-    expect(endtime.getTime() - starttime.getTime()).to.be.lessThan(250).and.gte(200)
+    expect(endtime.getTime() - starttime.getTime()).to.be.lessThan(sleeptime * 5).and.gte(sleeptime * 4)
   })
 
   it('eachConcurrent should default to a max concurrency of 10', async () => {
     const starttime = new Date()
-    await eachConcurrent(items, () => sleep(50))
+    await eachConcurrent(items, () => sleep(sleeptime))
     const endtime = new Date()
-    expect(endtime.getTime() - starttime.getTime()).to.be.lessThan(150).and.gte(100)
+    expect(endtime.getTime() - starttime.getTime()).to.be.lessThan(sleeptime * 3).and.gte(sleeptime * 2)
+  })
+
+  it('eachConcurrent should properly return values', async () => {
+    const doubles = await eachConcurrent(items, async (item) => item * 2)
+    expect(doubles).to.deep.equal(items.map(i => i * 2))
+  })
+
+  it('eachConcurrent should properly return values with in-flight limit', async () => {
+    const doubles = await eachConcurrent(items, 2, async (item) => item * 2)
+    expect(doubles).to.deep.equal(items.map(i => i * 2))
   })
 })
