d2855dd324fdddd45bd41863c6f4bdf34c9e7e27
Nick Wing <nwing@txstate.edu>
2020-07-24 16:11:22 -0500

    added isEmpty, isBlank, and hashify utility functions

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import dp from 'dot-prop'
+
 export function sleep (milliseconds: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds)
@@ -6,4 +8,44 @@ export function sleep (milliseconds: number) {
 
 export function randomid () {
   return String.fromCharCode(97 + Math.floor(Math.random() * 26)) + Math.random().toString(36).slice(2, 11)
+}
+
+export function isBlank (str: string|undefined|null) {
+  return !str || !str.trim || str.trim().length === 0
+}
+
+export function isEmpty (obj: any): boolean {
+  if (typeof obj === 'undefined' || obj === null) return true
+  if (typeof obj === 'number') return false
+  if (typeof obj === 'string') return isBlank(obj)
+  if (typeof obj.length === 'number') return !obj.length
+  if (typeof obj.isEmpty === 'function') return obj.isEmpty()
+  if (typeof obj.size === 'function') return !obj.size()
+  if (typeof obj === 'object') return !Object.keys(obj).length
+  return !obj
+}
+
+// export function hashify <ObjectType extends Object> (objArray: ObjectType[], key: string|number): Record<string|number, ObjectType>
+// export function hashify <ObjectType> (objArray: ObjectType[], extractKey: (obj: ObjectType) => string|number): Record<string|number, ObjectType>
+export function hashify <ObjectType extends Object, Key extends keyof ObjectType, KeyType extends (string|number)&ObjectType[Key]> (objArray: ObjectType[], key: Key): Record<KeyType, ObjectType>
+export function hashify <ObjectType, FuncReturnType extends string|number> (objArray: ObjectType[], keyOrExtractor: string|number|((obj: ObjectType) => FuncReturnType|undefined)): Record<FuncReturnType, ObjectType>
+export function hashify <ObjectType> (objArray: ObjectType[], keyOrExtractor: string|number|symbol|((obj: ObjectType) => string|number|undefined)) {
+  const hash: Record<string|number, ObjectType> = {}
+  if (typeof keyOrExtractor === 'function') {
+    for (const obj of objArray) {
+      const val = keyOrExtractor(obj)
+      if (val) hash[val] = obj
+    }
+  } else if (typeof keyOrExtractor === 'number' || typeof keyOrExtractor === 'symbol') {
+    for (const obj of objArray) {
+      const potentialkey = (obj as any)[keyOrExtractor]
+      if (potentialkey && ['string', 'number'].includes(typeof potentialkey)) hash[potentialkey] = obj
+    }
+  } else {
+    for (const obj of objArray) {
+      const potentialkey = dp.get(obj, keyOrExtractor) as string|number|undefined
+      if (potentialkey && ['string', 'number'].includes(typeof potentialkey)) hash[potentialkey] = obj
+    }
+  }
+  return hash
 }
--- test/util.ts
+++ test/util.ts
@@ -1,4 +1,5 @@
-import { sleep, randomid } from '../src'
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { sleep, randomid, hashify, isEmpty, isBlank } from '../src'
 import { expect } from 'chai'
 
 describe('sleep', () => {
@@ -17,5 +18,86 @@ describe('randomid', () => {
     const id = randomid()
     expect(id).to.match(/^[a-z]/)
     expect(id.length).to.be.gte(4)
+  })
+})
+
+describe('isBlank', () => {
+  it('should return true for null or undefined', () => {
+    expect(isBlank(null)).to.be.true
+    expect(isBlank(undefined)).to.be.true
+  })
+  it('should return true when only whitespace', () => {
+    expect(isBlank(' ')).to.be.true
+    expect(isBlank(' \t')).to.be.true
+  })
+  it('should return false when any non-whitespace characters are present in the string', () => {
+    expect(isBlank(' a')).to.be.false
+    expect(isBlank(' b\t')).to.be.false
+    expect(isBlank(' \tc')).to.be.false
+  })
+})
+
+describe('isEmpty', () => {
+  class ComplexObj {
+    protected list: any[]
+    constructor (list: any[]) {
+      this.list = list
+    }
+
+    public isEmpty () {
+      return !this.list.length
+    }
+  }
+  const emptyObj = new ComplexObj([])
+  const notEmptyObj = new ComplexObj([1, 2])
+  it('should work for plain objects', () => {
+    expect(isEmpty({})).to.be.true
+    expect(isEmpty({ hi: 'there' })).to.be.false
+  })
+  it('should work for arrays', () => {
+    expect(isEmpty([])).to.be.true
+    expect(isEmpty([1, 2])).to.be.false
+  })
+  it('should work for complex objects with an isEmpty method', () => {
+    expect(isEmpty(emptyObj)).to.be.true
+    expect(isEmpty(notEmptyObj)).to.be.false
+  })
+  it('should return false for the number 0', () => {
+    expect(isEmpty(0)).to.be.false
+  })
+})
+
+describe('hashify', () => {
+  const sym = Symbol('id')
+  const records = [
+    { [sym]: 1, idnum: 1, idstr: 'one', name: 'One', deep: { id: 1 } },
+    { [sym]: 2, idnum: 2, idstr: 'two', name: 'Two', deep: { id: 2 } },
+    { [sym]: 3, idnum: 3, idstr: 'three', name: 'Three', deep: { id: 3 } },
+    { [sym]: 4, idnum: 4, idstr: 'four', name: 'Four', deep: { id: 4 } }
+  ]
+  it('should work with a shallow string property that returns numbers', () => {
+    const hashed = hashify(records, sym)
+    expect(Object.keys(hashed)).to.have.lengthOf(4)
+    expect(hashed[1].name).to.equal('One')
+  })
+  it('should work with a shallow symbol property that returns numbers', () => {
+    const hashed = hashify(records, 'idnum')
+    expect(Object.keys(hashed)).to.have.lengthOf(4)
+    expect(hashed[1].name).to.equal('One')
+  })
+  it('should work with a shallow string properties that returns strings', () => {
+    const hashed = hashify(records, 'idstr')
+    expect(Object.keys(hashed)).to.have.lengthOf(4)
+    expect(hashed.one.name).to.equal('One')
+  })
+  it('should work with deep properties', () => {
+    const hashed = hashify(records, 'deep.id')
+    expect(Object.keys(hashed)).to.have.lengthOf(4)
+    expect(hashed[1].name).to.equal('One')
+  })
+  it('should work with an extractor function', () => {
+    const hashed = hashify(records, record => record.deep.id)
+    expect(Object.keys(hashed)).to.have.lengthOf(4)
+    expect(hashed[1].name).to.equal('One')
   })
 })
