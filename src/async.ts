4bb7e47352ffc9164966c4e922e77ed2d89ac3e3
Nick Wing <nwing@txstate.edu>
2021-07-23 19:26:10 -0500

    add a rescue function for easily  catching errors and setting a default

--- src/async.ts
+++ src/async.ts
@@ -11,3 +11,20 @@ export async function filterAsync<ItemType> (items: ItemType[], callback: (item:
   }))
   return items.filter((item, index) => bools[index])
 }
+
+/**
+ * modeled after rescue keyword from Ruby
+ *
+ * Provide a promise - if it successfully resolves the value is passed
+ * through, but if it rejects the error will be caught and the default value
+ * (or undefined) will be returned
+ */
+export async function rescue<ItemType> (promise: Promise<ItemType>): Promise<ItemType|undefined>
+export async function rescue<ItemType, DefaultType> (promise: Promise<ItemType>, defaultValue: DefaultType): Promise<ItemType|DefaultType>
+export async function rescue (promise: Promise<any>, defaultValue?: any) {
+  try {
+    return await promise
+  } catch {
+    return defaultValue
+  }
+}
--- test/async.ts
+++ test/async.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { expect } from 'chai'
-import { filterAsync } from '../src'
+import { filterAsync, rescue } from '../src'
 
 describe('async utils', () => {
   const items = Array.from(Array(20).keys())
@@ -11,5 +11,22 @@ describe('async utils', () => {
     for (const num of filtered) {
       expect(num % 2).to.equal(1)
     }
+  })
+
+  it('rescue returns the value when the promise is resolved', async () => {
+    const a = await rescue(new Promise<number>(resolve => resolve(2)))
+    expect(a).to.equal(2)
+  })
+  it('rescue returns the value when the promise is resolved, even if there is a default', async () => {
+    const a = await rescue(new Promise<number>(resolve => resolve(2)), 5)
+    expect(a).to.equal(2)
+  })
+  it('rescue returns undefined when the promise is rejected', async () => {
+    const a = await rescue(new Promise<number>((resolve, reject) => reject(new Error('rejected!'))))
+    expect(a).to.be.undefined
+  })
+  it('rescue returns a default value when the promise is rejected', async () => {
+    const a = await rescue(new Promise<number>((resolve, reject) => reject(new Error('rejected!'))), 5)
+    expect(a).to.equal(5)
   })
 })
