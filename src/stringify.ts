494338e05f38e4972a6502fbe2cfc5041b0fcef9
Nick Wing <nwing@txstate.edu>
2022-04-07 15:44:09 -0500

    add stable stringify function

---
+++ src/stringify.ts
@@ -0,0 +1,51 @@
+/**
+ * A stable stringify function that sorts object keys before stringifying
+ * so that identical objects with different key ordering will stringify to identical
+ * strings.
+ *
+ * If the object being stringified contains cycles, the deeper appearance
+ * of the cycled object will be replaced with the string '__cycle__'
+ */
+export function stringify (data: any) {
+  const seen: any[] = []
+  return (function stringify (node: any) {
+    if (typeof node?.toJSON === 'function') {
+      node = node.toJSON()
+    }
+
+    if (node === undefined) return
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+    if (typeof node === 'number') return isFinite(node) ? '' + node : 'null'
+    if (typeof node !== 'object') return JSON.stringify(node)
+
+    let i, out
+    if (Array.isArray(node)) {
+      out = '['
+      for (i = 0; i < node.length; i++) {
+        if (i) out += ','
+        out += stringify(node[i]) ?? 'null'
+      }
+      return out + ']'
+    }
+
+    if (node === null) return 'null'
+
+    if (seen.includes(node)) {
+      return JSON.stringify('__cycle__')
+    }
+
+    const seenIndex = seen.push(node) - 1
+    const keys = Object.keys(node).sort()
+    out = ''
+    for (i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      const value = stringify(node[key])
+
+      if (!value) continue
+      if (out) out += ','
+      out += JSON.stringify(key) + ':' + value
+    }
+    seen.splice(seenIndex, 1)
+    return '{' + out + '}'
+  })(data)
+};
---
+++ test/stringify.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+import { stringify } from '../lib'
+
+describe('intersect', function () {
+  it('should work for a scalar', () => {
+    expect(stringify('test')).to.equal(JSON.stringify('test'))
+    expect(stringify(2)).to.equal(JSON.stringify(2))
+  })
+  it('should work for a deep object', () => {
+    const obj = { should: { be: 'deep' } }
+    expect(stringify(obj)).to.equal(JSON.stringify(obj))
+  })
+  it('should provide consistent key ordering', () => {
+    const obj1 = { test: 'object', should: { be: 'deep' } }
+    const obj2 = { should: { be: 'deep' }, test: 'object' }
+    expect(stringify(obj1)).to.equal(stringify(obj2))
+    expect(JSON.stringify(obj1)).to.not.equal(JSON.stringify(obj2))
+  })
+  it('should not reorder arrays', () => {
+    const obj = { array: [5, 4, 3, 2, 1] }
+    expect(stringify(obj)).to.equal(JSON.stringify(obj))
+  })
+})
