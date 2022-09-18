7bfc212eeb27d9396216dd845983dbb3ccb95ab2
Nick Wing <nwing@txstate.edu>
2022-09-18 05:59:13 -0500

    extra overloads for toArray to help with cases where you allow optional pairs

--- src/array/toarray.ts
+++ src/array/toarray.ts
@@ -6,6 +6,9 @@
  * provide a singular scalar for convenience, e.g.:
  * cache.findById(53) and cache.findById([53. 78, 99])
  */
+export function toArray (val: null | undefined): []
+export function toArray <T, U> (val: T | [T] | [T, U]): [T] | [T, U]
+export function toArray <T> (val: T | T[] | undefined | null): T[]
 export function toArray <T> (val: T | T[] | undefined | null) {
   val ??= []
   if (Array.isArray(val)) return val
