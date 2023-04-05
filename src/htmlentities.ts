ded6b2704efaba57c8a34d84711326ad6d5f79a4
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-04-05 14:06:54 -0500

    Updated to fix comment added to htmlEncode with clearer language.

--- src/htmlentities.ts
+++ src/htmlentities.ts
@@ -10,12 +10,12 @@ const escapes: Record<string, string> = {
 const badChars = /[&<>"'`=]/g
 function escapeChar (c: string) { return escapes[c] }
 
-/** Replaces characters in `str` to their HTML encoded escape values.
- * Returns an empty string if `str` is `undefined` or `null`.
+/** Returns the result of replacing HTML reserved characters in `str` with their HTML encoded escape values.
+ * @returns an empty string if `str` is `undefined` or `null`.
  * @example
  * ```ts
  * htmlEncode('Text with WAVE breaking <h1>text</h1> in it.')
- * // Returns: 'Text with WAVE breaking &lt;h1&gt;text&lt;/h1&gt; in it.
+ * // Returns: 'Text with WAVE breaking &lt;h1&gt;text&lt;/h1&gt; in it.'
  * ``` */
 export function htmlEncode (str: string | undefined | null) {
   if (str == null) return ''
