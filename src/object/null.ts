07d25596145b4f134aae9f8d4b006917d5a1718a
Nick Wing <nwing@txstate.edu>
2024-03-27 15:58:25 -0500

    fix for destroyNulls to avoid crashing, release 1.9.2

--- src/object/null.ts
+++ src/object/null.ts
@@ -3,7 +3,7 @@ import type { DestroyNulls } from '../typeutils'
 export function destroyNulls<T> (obj: T): DestroyNulls<T> {
   if (obj == null) return undefined as any
 
-  if (obj.constructor.name === 'Object') {
+  if (typeof obj === 'object') {
     for (const key in obj) {
       obj[key] = destroyNulls(obj[key]) as any
     }
